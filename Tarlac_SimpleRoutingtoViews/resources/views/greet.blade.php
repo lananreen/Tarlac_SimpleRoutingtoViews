c2dcd04bef34da5d72dca60f4dfa213969cdd103
lananreen <lananreen@gmail.com>
2026-02-16 00:01:51 +0000

    Laravel activity in codespace

---
+++ Tarlac_SimpleRoutingtoViews/resources/views/greet.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Greeting Page</title>
+</head>
+<body>
+    <h1>Welcome to the Greet View!</h1>
+    <p>This page is being rendered via the GreetController.</p>
+</body>
+</html>
